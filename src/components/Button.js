2b7dc4f4c6874711258ec94639435f537b3f0cb4
Amanda Beiner <aebeiner@gmail.com>
2016-10-31 11:26:08 -0400

    preliminary styling

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,13 +6,21 @@ const Button = props => {
 
   return(
     <div>
-      <button onClick={props.handleNewWordClick}>
-        New Word
-      </button>
+      <div className="button-space">
+        <h1>Welcome to Fridge Magnets</h1>
+        <h2>Click to add new words to your magnetic poetry</h2>
 
-      <button>
-        Clear
-      </button>
+        <button className='btn' onClick={props.handleNewWordClick}>
+          New Word
+        </button>
+
+        <button className='btn' onClick={props.handleClearClick}>
+          Clear
+        </button>
+
+        </div>
+
+        <img src= 'https://s13.postimg.org/to254ablj/refrigerator.png'></img>
     </div>
   )
 }
